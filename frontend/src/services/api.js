2627cf00786793cd3d01698e5fc154eb46f5eff6
niteshvshukla2026-sudo <niteshshukla56@gmail.com>
2026-01-10 13:21:52 +0530

    api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL + "/api",
 });
 
+// 🔐 TOKEN AUTO ATTACH
 API.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
